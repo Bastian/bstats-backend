7544bf9246842c9d1c0a34703a3fe380e739ba9f
Bastian <bastianoppermann1997@gmail.com>
2020-11-25 15:02:32 +0100

    Simplify JAVA_VERSION_MAPPING

--- src/data-submission/parser/java-version.parser.ts
+++ src/data-submission/parser/java-version.parser.ts
@@ -12,21 +12,10 @@ export class JavaVersionParser implements Parser {
   private JAVA_VERSION_MAPPING = [
     ['1.7', 'Java 7'],
     ['1.8', 'Java 8'],
-    //java 9 changed the version format to 9.0.1 and 1.9.0 is only used for early access
-    //reference: http://openjdk.java.net/jeps/223
-    ['1.9.0-ea', 'Java 9'],
-    ['9', 'Java 9'],
-    ['10', 'Java 10'],
-    ['11', 'Java 11'],
-    ['12', 'Java 12'],
-    ['13', 'Java 13'],
-    ['14', 'Java 14'],
-    ['15', 'Java 15'],
-    ['16', 'Java 16'],
-    ['17', 'Java 17'],
-    ['18', 'Java 18'],
-    ['19', 'Java 19'],
-    ['20', 'Java 20'],
+    // Java 9 changed the version format to 9.x.x instead of 1.9.x
+    ...[...Array(22).keys()] // [0..22]
+      .map((i) => i + 9) // [9..30]
+      .map((i) => [`${i}`, `Java ${i}`]),
   ];
 
   parse(
