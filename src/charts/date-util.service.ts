9e2d1aa3df748c080df9953749f91d159b885fb8
Bastian <bastianoppermann1997@gmail.com>
2020-12-02 11:51:02 +0100

    Use UTC time

--- src/charts/date-util.service.ts
+++ src/charts/date-util.service.ts
@@ -2,19 +2,19 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class DateUtilService {
+  private PAST = Date.UTC(2000, 1, 1, 0, 0, 0, 0);
+
   /**
    * Converts a date to a tms2000 value.
    */
   dateToTms2000(date: Date): number {
-    const past = new Date(2000, 1, 1, 0, 0, 0, 0).getTime();
-    return ((date.getTime() - past) / (1000 * 60 * 30)) | 0;
+    return ((date.getTime() - this.PAST) / (1000 * 60 * 30)) | 0;
   }
 
   /**
    * Converts tms2000 tp date.
    */
   tms2000ToDate(tms2000: number): Date {
-    const past = new Date(2000, 1, 1, 0, 0, 0, 0).getTime();
-    return new Date(past + tms2000 * 1000 * 60 * 30);
+    return new Date(this.PAST + tms2000 * 1000 * 60 * 30);
   }
 }
