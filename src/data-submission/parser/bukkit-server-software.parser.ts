c13ab91caa3e1740dd22587d9ec33544d7270ff7
heipiao233 <heipiao233@outlook.com>
2022-06-13 20:55:54 +0800

    Support Arclight

--- src/data-submission/parser/bukkit-server-software.parser.ts
+++ src/data-submission/parser/bukkit-server-software.parser.ts
@@ -50,6 +50,8 @@ export class BukkitServerSoftwareParser implements Parser {
       softwareName = 'Airplane';
     } else if (lowercaseBukkitVersion.indexOf('yatopia') !== -1) {
       softwareName = 'Yatopia';
+    } else if (lowercaseBukkitVersion.indexOf('Arclight') !== -1) {
+      softwareName = 'Arclight';
     }
 
     return [
