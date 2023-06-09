cd47ff45be491492c86b5e5dd1e7d1de452c2051
Telesphoreo <me@telesphoreo.me>
2023-06-08 20:10:23 -0500

    Add Scissors

--- src/data-submission/parser/bukkit-server-software.parser.ts
+++ src/data-submission/parser/bukkit-server-software.parser.ts
@@ -56,6 +56,8 @@ export class BukkitServerSoftwareParser implements Parser {
       softwareName = 'Magma';
     } else if (lowercaseBukkitVersion.indexOf('titanium') !== -1) {
       softwareName = "Titanium";
+    } else if (lowercaseBukkitVersion.indexOf('scissors') !== -1 {
+      softwareName = "Scissors";
     }
 
     return [
